18c18f84245826a059fd57b7af3c0e27c67fcfa3
Pancw2000 <panchuwen2000@gmail.com>
2022-05-02 02:25:43 +0800

    deleted some files

--- newaxis.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-A = np.arange(4)
-B = A[np.newaxis, :]
-print(B)
-C = A[: ,np.newaxis]
-print(C)
--- open_image.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import numpy as np
-
-image = Image.open('cat.png')
-ycbcr = image.convert('YCbCr')
-npmat = np.array(ycbcr, dtype=np.uint8)
-print(npmat.shape)
-image_back = Image.fromarray(npmat,'YCbCr')
-image_back.show()
